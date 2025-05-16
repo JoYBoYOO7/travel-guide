69af306816737548dcf893de1adaafb2ddad1102
Vansh Yadav <vansh.15926@gmail.com>
2025-05-16 14:47:18 +0530

    dashboard ui, auth functionality

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,16 @@
+import { Header } from 'components'
 import React from 'react'
 
 const AllUsers = () => {
   return (
-    <div>Users Table</div>
+    <main className='dashboard wrapper'>
+      <Header
+        title="Trips Page"
+        description="check out our users and their trips"
+      />
+
+      ALL USERS Page content
+    </main>
   )
 }
 
