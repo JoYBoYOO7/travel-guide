aeae9344e9b548c50b4a84938eb9a2a4b6af2dea
Vansh Yadav <vansh.15926@gmail.com>
2025-05-17 01:06:44 +0530

    implemented travel page

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,17 +1,52 @@
 import { PagerComponent } from "@syncfusion/ej2-react-grids"
 import { Header, TripCard } from "components"
+import { parseTripData } from "lib/utils";
+import { useState } from "react";
+import { useSearchParams, type LoaderFunctionArgs } from "react-router";
+import { getAllTrips } from "~/appwrite/trips";
+import type { Route } from "./+types/trips";
 
-const trips = () => {
-  return (
-    <main className="all-users wrapper">
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+    const limit = 8;
+    const url = new URL(request.url);
+    const page = parseInt(url.searchParams.get('page') || "1", 10);
+    const offset = (page - 1) * limit;
+
+    const { allTrips, total } = await getAllTrips(limit, offset);
+
+    return {
+        trips: allTrips.map(({ $id, tripDetail, imageUrls }) => ({
+            id: $id,
+            ...parseTripData(tripDetail),
+            imageUrls: imageUrls ?? []
+        })),
+        total
+    }
+}
+
+const Trips = ({ loaderData }: Route.ComponentProps) => {
+    const trips = loaderData.trips as Trip[] | [];
+
+    const [searchParams] = useSearchParams();
+    const initialPage = Number(searchParams.get('page') || '1')
+
+    const [currentPage, setCurrentPage] = useState(initialPage);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+        window.location.search = `?page=${page}`
+    }
+
+    return (
+        <main className="all-users wrapper">
             <Header
                 title="Trips"
-                description="View and Edit AI-generated travel plans"
+                description="View and edit AI-generated travel plans"
                 ctaText="Create a trip"
                 ctaUrl="/trips/create"
             />
 
-            {/* <section>
+            <section>
                 <h1 className="p-24-semibold text-dark-100 mb-4">
                     Manage Created Trips
                 </h1>
@@ -37,9 +72,8 @@ const trips = () => {
                     click={(args) => handlePageChange(args.currentPage)}
                     cssClass="!mb-4"
                 />
-            </section> */}
+            </section>
         </main>
-  )
+    )
 }
-
-export default trips
+export default Trips
